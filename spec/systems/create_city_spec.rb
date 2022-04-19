eec285d0dc47f5f053bb0dbe64fcd91a8b164ecc
netng <net.nandang@gmail.com>
2022-04-19 13:14:31 +0700

    updating create_city_spec when invalid input

--- spec/systems/create_city_spec.rb
+++ spec/systems/create_city_spec.rb
@@ -8,4 +8,11 @@ RSpec.describe "Creating a city", type: :system do
         visit cities_path
         expect(page).to have_content("Bogor")
     end
+
+    scenario "invalid input" do
+        visit new_city_path
+        fill_in "Name", with: ""
+        click_on "Create City"
+        expect(page).to have_content("Name can't be blank")
+    end
 end
